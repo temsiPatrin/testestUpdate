ee1a124820a37ba3414ef5e84f44e7306e550eb3
artempatrin <patrik2207art@gmail.com>
2020-10-16 15:42:11 +0300

    version2

--- app/src/main/java/com/temsi/testupdate/MainActivity.kt
+++ app/src/main/java/com/temsi/testupdate/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.temsi.testupdate
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.javiersantos.appupdater.AppUpdater
+import com.github.javiersantos.appupdater.enums.UpdateFrom
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val appUpdater = AppUpdater(this)
+                .setUpdateFrom(UpdateFrom.GITHUB)
+                .setGitHubUserAndRepo("temsiPatrin","testestUpdate")
+            appUpdater.start()
+        }
     }
+
 }
